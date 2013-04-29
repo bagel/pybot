6d8ed51447c87d9457b7c62b89b54260fadf4f78
bagel <freetgm@gmail.com>
2013-04-29 22:10:24 +0800

    change

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import os
 import HTMLParser
 
 
-class htmlparser(HTMLParser.HTMLParser):
+class Htmlparser(HTMLParser.HTMLParser):
     def __init__(self, th=[], td=[]):
         HTMLParser.HTMLParser.__init__(self)
         self.th = th
@@ -28,30 +28,31 @@ class htmlparser(HTMLParser.HTMLParser):
 
     def handle_data(self, data):
         if self.t == 1:
-            self.th.append(data)
+            self.th.append(data)  #监测项
             self.t = 0
         if self.br == 1:
             self.th[-1] = self.th[-1] + " " + data
             self.br = 0
         if self.pos == 1:
-            self.td.append(data)
+            self.td.append(data)  #监测值
             self.i += 1
         if self.i == 9:
             self.pos = 0
-        if data == '海淀区万柳':
+        if data == '海淀区万柳':  #监测点
             self.td.append(data)
             self.pos = 1
         if re.search('数据更新时间', data):
             self.th.append(data.split('：')[0])
             self.td.append(data.split('：')[1])
 
-def aqi():
-    text = ''.join([ line.strip() for line in urllib2.urlopen(url='http://www.pm25.in/beijing', timeout=5).readlines() if re.search('<th.*>|<td.*>|<p>', line) ])
-    #print text
-    #text = "<html><body><td>text</td></body></html>"
-    th = []
-    td = []
-    parser = htmlparser(th=th, td=td)
-    parser.feed(text)
-    return '\n'.join([ "%s:  %s" % (h, td[th.index(h)]) for h in th ])
+class Weather:
+    def aqi(self):
+        text = ''.join([ line.strip() for line in urllib2.urlopen(url='http://www.pm25.in/beijing', timeout=5).readlines() if re.search('<th.*>|<td.*>|<p>', line) ])
+        #print text
+        #text = "<html><body><td>text</td></body></html>"
+        th = []
+        td = []
+        parser = Htmlparser(th=th, td=td)
+        parser.feed(text)
+        return '\n'.join([ "%s:  %s" % (h, td[th.index(h)]) for h in th ])
     
